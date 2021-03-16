c64c43f6a781fa5668de843fa6d516f07a002737
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 20:05:47 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,16 +1,7 @@
 <?php
 
-   $host        = "host=ec2-54-145-249-177.compute-1.amazonaws.com";
-   $port        = "port=5432";
-   $dbname      = "dbname = d9or597utqf8fr";
-   $credentials = "user = avhuqttulgfnie password=89cb66cc15fb996c1a7336cd971e27484e8f5499e9f21390d641343d0e727a55";
-
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      //echo "Opened database successfully\n";
-   }
+   $db = pg_connect("host=ec2-54-145-249-177.compute-1.amazonaws.com port=5432 dbname=d9or597utqf8fr user=avhuqttulgfnie password=89cb66cc15fb996c1a7336cd971e27484e8f5499e9f21390d641343d0e727a55");
+if(isset($_POST['submit'])){
 
 
 
@@ -27,5 +18,6 @@
       echo "Records created successfully\n";
    }
    pg_close($db);
+}
 ?>
 
