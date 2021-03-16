55ca9f910c90b3914b962712627094ffa3727e9d
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 17:29:48 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-   $host        = "host=127.0.0.1";
+   $host        = "host=ec2-54-145-249-177.compute-1.amazonaws.com";
    $port        = "port=5432";
-   $dbname      = "dbname = testdb";
-   $credentials = "user = postgres password=pass123";
+   $dbname      = "dbname = d9or597utqf8fr";
+   $credentials = "user = avhuqttulgfnie password=89cb66cc15fb996c1a7336cd971e27484e8f5499e9f21390d641343d0e727a55";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
