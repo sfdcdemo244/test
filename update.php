108ffec689f3f1a3328c0562c5bcb94eb2816ac5
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 21:21:45 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 
    $db = pg_connect("host=ec2-54-145-249-177.compute-1.amazonaws.com port=5432 dbname=d9or597utqf8fr user=avhuqttulgfnie password=89cb66cc15fb996c1a7336cd971e27484e8f5499e9f21390d641343d0e727a55");
-if(isset($_POST['submit'])){
+if(isset($_POST['update'])){
 
 
 $fname=$_POST['fname'];
