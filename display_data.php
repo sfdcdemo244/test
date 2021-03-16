fe376d3455eb19b6c5f5eb2eb88bd62d808c6cb7
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 18:27:05 +0530

    Update display_data.php

--- display_data.php
+++ display_data.php
@@ -38,7 +38,7 @@ tr:nth-child(even) {
   
   <?php
 $db = pg_connect("host=ec2-54-145-249-177.compute-1.amazonaws.com port=5432 dbname=d9or597utqf8fr user=avhuqttulgfnie password=89cb66cc15fb996c1a7336cd971e27484e8f5499e9f21390d641343d0e727a55");
-$result = pg_query($db,"SELECT firstname,lastname,email,phone FROM salesforce.contact");
+$result = pg_query($db,"SELECT id,firstname,lastname,email,phone FROM salesforce.contact");
 $cn=1;
 while($row=pg_fetch_assoc($result)){ 
   ?>
