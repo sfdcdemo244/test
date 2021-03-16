c05fa66519d402b0563e04bc3dc8f19dc8e115a7
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 19:44:33 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@ include('connection.php');
 
 $query = "INSERT INTO salesforce.contact(firstname,lastname,email,phone) VALUES ($_POST['fname'], $_POST['lname'], $_POST['email'], $_POST['mob'])";
 
-$result = pg_query($query); 
+$result = pg_query($db,$query); 
 
 
 echo"print";
