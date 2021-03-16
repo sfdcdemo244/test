21f55311dbe0a708df72b7ee6c0a81d426279e49
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 17:41:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -60,29 +60,58 @@ a {
   </head>
   <body>
     
-    <form action="action_page.php">
+    <form action="#" method="POST">
   <div class="container">
     <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
+    <p>Please fill in this form to create an Contact.</p>
     <hr>
-
+<label for="email"><b>FirstName</b></label>
+    <input type="text" placeholder="Enter FirstName" name="fname" id="fname" required>
+   
+    <label for="email"><b>LastName</b></label>
+    
+    <input type="text" placeholder="Enter LastName" name="lname" id="lname" required>
+    
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+    <label for="psw"><b>Mobile Number</b></label>
+    <input type="number" placeholder="Enter MobileNo" name="mob" id="mob" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+  
     <hr>
 
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-    <button type="submit" class="registerbtn">Register</button>
+    <button type="submit" class="registerbtn" name="submit">Submit</button>
   </div>
 
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
-  </div>
+ 
 </form>
   </body>
 </html>
+
+
+<?php
+if(isset($_POST['submit'])){
+  
+  $fname=$_POST['fname'];
+   $lname=$_POST['lname'];
+   $email=$_POST['email'];
+   $mob=$_POST['mob'];
+
+  $sql =
+      INSERT INTO salesforce.contact (firstname,lastname,email,phone)
+      VALUES ($fname,$lname,$email,$mob);
+
+     
+
+   $ret = pg_query($db, $sql);
+   if(!$ret) {
+      echo pg_last_error($db);
+   } else {
+      echo "<script>window.alert('Record Created');</script>";
+   }
+   pg_close($db);
+?>
+
+
